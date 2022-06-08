306a54190f74203d02ad3836b87af2dfb9a5dd17
Saad Niazi <saadniazifed@gmail.com>
2022-06-08 16:55:42 +0500

    Length Tests are working
    With the help of triangulation, the 3 tests for lengths have been passed.

--- src/shipFactory.js
+++ src/shipFactory.js
@@ -1,5 +1,11 @@
 function Ship(length) {
+  //Keeping the actual array private
+  const _shipArray = Array(length).fill(null);
+
+  //Making a public method.
+  const getShipArray = () => [..._shipArray];
   return {
+    getShipArray,
     length,
   };
 }
--- src/tests/ship.test.js
+++ src/tests/ship.test.js
@@ -4,6 +4,17 @@ test("Is Ship Returning Us Something", () => {
   expect(Ship()).toBeDefined();
 });
 
-test("Ship Length Test", () => {
-  expect(Ship(3)).toHaveLength(3);
+test("Correct length of Ship", () => {
+  const myFirstShip = Ship(3);
+  expect(myFirstShip.length).toEqual(3);
+});
+
+test("Correct length of Ship #2", () => {
+  const mySecondShip = Ship(1);
+  expect(mySecondShip.length).toEqual(1);
+});
+
+test("Correct length of Ship #3", () => {
+  const myThirdShip = Ship(2);
+  expect(myThirdShip.length).toEqual(2);
 });
