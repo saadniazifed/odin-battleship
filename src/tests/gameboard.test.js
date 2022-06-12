9536167aa82c4d52e0c66196e9337e46e9b0e04c
Saad Niazi <saadniazifed@gmail.com>
2022-06-12 20:37:59 +0500

    Removed Testing Bug #5
    Removed the bug which was causing Water Carrier ships to be placed at multiple indexes.

--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -10,37 +10,18 @@ test("Checking to see if the Gameboard factory function returns us something", (
   expect(Gameboard()).toBeDefined();
 });
 
-test("Placing a Water Carrier Ship at a specific coordinate", () => {
-  expect(
-    Gameboard().placeShip().placeWaterCarrier(0, 0, "horizontal")[0][0]
-  ).toEqual("Water Carrier");
-  expect(
-    Gameboard().placeShip().placeWaterCarrier(0, 1, "horizontal")[0][1]
-  ).toEqual("Water Carrier");
-  expect(
-    Gameboard().placeShip().placeWaterCarrier(0, 2, "horizontal")[0][2]
-  ).toEqual("Water Carrier");
-  expect(
-    Gameboard().placeShip().placeWaterCarrier(0, 3, "horizontal")[0][3]
-  ).toEqual("Water Carrier");
-  expect(
-    Gameboard().placeShip().placeWaterCarrier(0, 4, "horizontal")[0][4]
-  ).toEqual("Water Carrier");
+describe.only("Water Carrier Ship Tests are inside this block", () => {
+  test("Placement of Water Carrier Ship", () => {
+    gameboard.placeShip().placeWaterCarrier(0, 0, "horizontal");
+
+    expect(gameboard.gameBoardArray[0][0]).toEqual("Water Carrier");
+    expect(gameboard.gameBoardArray[1][0]).toEqual("Water Carrier");
+    expect(gameboard.gameBoardArray[2][0]).toEqual("Water Carrier");
+    expect(gameboard.gameBoardArray[2][0]).toEqual("Water Carrier");
+  });
 });
 
-test("Placing a Destroyer Ship at a specific coordinate", () => {
-  expect(
-    Gameboard().placeShip().placeDestroyer(0, 2, "horizontal")[0][2]
-  ).toEqual("Destroyer");
-  expect(
-    Gameboard().placeShip().placeDestroyer(0, 3, "horizontal")[0][3]
-  ).toEqual("Destroyer");
-  expect(
-    Gameboard().placeShip().placeDestroyer(0, 4, "horizontal")[0][4]
-  ).toEqual("Destroyer");
-});
-
-describe.only("Destroyer Ship Tests are inside this block", () => {
+describe("Destroyer Ship Tests are inside this block", () => {
   test("Placement of Destroyer Ship", () => {
     gameboard.placeShip().placeDestroyer(0, 0, "horizontal");
 
