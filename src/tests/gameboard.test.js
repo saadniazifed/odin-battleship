9d190ab1161f5a345975f6c551260185913d82b4
Saad Niazi <saadniazifed@gmail.com>
2022-06-17 00:44:07 +0500

    directionOfShips function checking for ship length 3 (horizontal)

--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -410,6 +410,25 @@ describe.only("Checking the directionOfShips function with Ship Length 2", () =>
   });
 });
 
+describe.only("Checking the directionOfShips function with Ship Length 3", () => {
+  test("Check the horizontal direction", () => {
+    const myFirstShip = Ship(3);
+    expect(
+      gameboard.directionOfShips([0, 0], myFirstShip, "horizontal")
+    ).toEqual(true);
+    expect(
+      gameboard.directionOfShips([1, 0], myFirstShip, "horizontal")
+    ).toEqual(true);
+    expect(
+      gameboard.directionOfShips([2, 0], myFirstShip, "horizontal")
+    ).toEqual(true);
+
+    expect(
+      gameboard.directionOfShips([0, 1], myFirstShip, "horizontal")
+    ).toEqual(false);
+  });
+});
+
 // describe("Checking the directionOfShips function with Ship Length 4", () => {
 //   test("Check the horizontal direction", () => {
 //     const myFirstShip = Ship(4);
