d2663ab3a84aaa537e3fd17a143fb0c1f97b538e
Saad Niazi <saadniazifed@gmail.com>
2022-06-17 22:30:59 +0500

    isSunk method is working correctly on the true value on ship length 5

--- src/tests/ship.test.js
+++ src/tests/ship.test.js
@@ -141,3 +141,16 @@ describe("Testing the isSunk function in the ship of length 4", () => {
     expect(myFirstShip.isSunk()).toEqual(false);
   });
 });
+
+describe("Testing the isSunk function in the ship of length 5", () => {
+  test("Testing hit function", () => {
+    const myFirstShip = Ship(5);
+    myFirstShip.hit();
+    myFirstShip.hit();
+    myFirstShip.hit();
+    myFirstShip.hit();
+    myFirstShip.hit();
+
+    expect(myFirstShip.isSunk()).toEqual(true);
+  });
+});
