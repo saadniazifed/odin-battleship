74398fad011c9b6849df73d94f4a458cfc0790f2
Saad Niazi <saadniazifed@gmail.com>
2022-06-18 13:15:34 +0500

    Using object.assign to pass in the methods of Player factory function and testing getPlayerNum method

--- src/tests/player.test.js
+++ src/tests/player.test.js
@@ -74,3 +74,10 @@ describe.only("Checking if Computer Player has a name ", () => {
     expect(computer.getName()).toEqual("Computer");
   });
 });
+
+describe.only("Checking if Computer Player has a playerNum ", () => {
+  test("Checking for the ComputerPlayer factory function getPlayerNum property", () => {
+    const computer = ComputerPlayer("Computer", 2);
+    expect(computer.getPlayerNum()).toEqual(2);
+  });
+});
